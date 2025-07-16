7b8f982b537a1028a8749c1ab32cb28e323fef56
Amrazyan1 <arman.amrazyan@softconstruct.com>
2025-07-16 13:03:17 +0400

    test stack

--- DeviceManager.cs
+++ DeviceManager.cs
@@ -41,7 +41,7 @@ public class DeviceManager : IDisposable
             Console.WriteLine($"DeviceManager InitDevice() Device on port {port.Name} initialized.");
 
             _initMEI(this.currentDevice, MEIInstruction.InitExtScaScrAndPoll);
-            // StartPolling();
+            StartPolling();
         }
         catch (Exception ex)
         {
@@ -267,6 +267,69 @@ public class DeviceManager : IDisposable
 
         MEICommand stdHostToAcc = new MEICommand(MEIInstruction.StdHostToAcc, 0, 128);
 
+
+        while (pollOn)
+        {
+            try
+            {
+                outLen = stdHostToAcc.RunOn(device);
+
+                // Check whether the received response is a standard Acceptor to Host message
+                if ((stdHostToAcc.OutputBuffer[0] & 0xF0) == (int)MEIInstruction.StdAccToHost)
+                {
+                    // In this case, the status data bytes are retrieved from the second index in the array
+                    if (outLen >= 5 && (((MeiStatus)BitConverter.ToUInt32(stdHostToAcc.OutputBuffer, 1)) & MeiStatus.Escrowed) == Quixant.LibRAV.MeiStatus.Escrowed)
+                    {
+                        Console.WriteLine("Received escrowed event of a bank note");
+                        int denomination = (stdHostToAcc.OutputBuffer[3] & 0x38) >> 3; // Bits 3-5 represent the denomination
+                        Console.WriteLine("Denomination: " + denomination);
+                        Console.WriteLine("Sending stack command...");
+                        Thread.Sleep(1000);
+
+                        // stack.RunOn(device);
+                    }
+                    else if (outLen >= 5 && BitConverter.ToUInt16(stdHostToAcc.OutputBuffer, 1) != 0x1001)
+                    {
+                        uint statusValue = BitConverter.ToUInt32(stdHostToAcc.OutputBuffer, 1);
+                        Console.WriteLine("Received status: " + statusValue.ToString("X8"));
+                    }
+                }
+                else if ((stdHostToAcc.OutputBuffer[0] & 0xF0) == (int)MEIInstruction.ExtendedMsgSet)
+                {
+                    // Switch the extended command subtype
+                    switch (stdHostToAcc.OutputBuffer[1])
+                    {
+                        case (byte)MEIMessageExtendedSubtype.ExtendedBarcodeReply:
+                            {
+                                Console.WriteLine("Received escrowed event of a ticket");
+                                // The extended data field for Barcodes is 28 bytes long and represented in ASCII.
+                                // The Barcode data is left justified LSC (Least Significant Character)
+                                // and all unused bytes are filled with 0x28.
+                                // First 8 bytes are:
+                                // Message type + Sybtype + Status data (4 bytes) + Model# + Revision#
+                                Console.Write("Barcode value: ");
+                                for (int i = 8; i < 28 && stdHostToAcc.OutputBuffer[i] != 0x28; i++)
+                                {
+                                    Console.Write((char)stdHostToAcc.OutputBuffer[i]);
+                                }
+                                Console.WriteLine();
+                                Console.WriteLine("Sending stack command...");
+                                Thread.Sleep(1000);
+                                // stack.RunOn(device);
+                                break;
+                            }
+                        default:
+                            break;
+                    }
+                }
+            }
+            catch (Exception exc)
+            {
+                Console.WriteLine("\nGet error: " + exc.Message);
+            }
+            Thread.Sleep(200);
+        }
+        return;
         // Poll the device each 200 ms 
         while (pollOn)
         {

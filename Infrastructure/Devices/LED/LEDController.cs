545907715b19b2a7cf55a3794850b4ed0b780514
Amrazyan1 <arman.amrazyan@softconstruct.com>
2025-07-22 15:42:07 +0400

    [LED] Minor cleaning

--- Infrastructure/Devices/LED/LEDController.cs
+++ Infrastructure/Devices/LED/LEDController.cs
@@ -3,7 +3,7 @@ using Quixant.LibQLed;
 
 public class LEDController : IDisposable
 {
-    private QxLedController _controller;
+    private QxLedController? _controller;
     protected readonly Dictionary<int, CancellationTokenSource> _channelLoops = new();
 
     public void Init()
@@ -15,9 +15,13 @@ public class LEDController : IDisposable
     public void ApplyPattern(int ch, ILedPattern pattern)
     {
         StopLoop(ch);
-        var cts = new CancellationTokenSource();
-        _channelLoops[ch] = cts;
-        _ = pattern.StartAsync(ch, _controller, cts);
+
+        if (_controller != null)
+        {
+            var cts = new CancellationTokenSource();
+            _channelLoops[ch] = cts;
+            _ = pattern.StartAsync(ch, _controller, cts);
+        }
     }
 
     public void DisposeController()
@@ -34,59 +38,6 @@ public class LEDController : IDisposable
         this.ApplyPattern(1, new HearthbeatPattern(Color.Red));
         this.ApplyPattern(2, new LoopFadePattern());
         this.ApplyPattern(3, new RainbowPattern());
-    }
-
-    public async Task FadeColor(int ch, Color targetColor, float duration)
-    {
-        StopLoop(ch);
-        if (_controller != null)
-        {
-            await _controller.FadeToColor(ch, ToByte(targetColor.R), ToByte(targetColor.G), ToByte(targetColor.B),
-                duration);
-        }
-    }
-
-    public async Task ChaseColor(int ch, Color color, float delayPerLed)
-    {
-        StopLoop(ch);
-        if (_controller != null)
-        {
-            await _controller.Chase(ch, ToByte(color.R), ToByte(color.G), ToByte(color.B), delayPerLed);
-        }
-    }
-
-    public void Clear(int ch)
-    {
-        StopLoop(ch);
-        _controller?.ClearChannel(ch);
-    }
-
-    public void ClearAll()
-    {
-        for (int ch = 0; ch < 4; ch++)
-            Clear(ch);
-    }
-
-    public async void LoopChase(int ch, Color color, float delayPerLed)
-    {
-        StopLoop(ch);
-        var cts = new CancellationTokenSource();
-        _channelLoops[ch] = cts;
-        await LoopChaseAsync(ch, color, delayPerLed, cts.Token);
-    }
-
-    private async Task LoopChaseAsync(int ch, Color color, float delay, CancellationToken token)
-    {
-        try
-        {
-            while (!token.IsCancellationRequested)
-            {
-                await ChaseColor(ch, color, delay);
-            }
-        }
-        catch (OperationCanceledException)
-        {
-        }
     }
 
     public void StopLoop(int ch)
@@ -107,11 +58,8 @@ public class LEDController : IDisposable
             cts.Cancel();
             cts.Dispose();
         }
-
         _channelLoops.Clear();
     }
-
-    private static byte ToByte(float f) => (byte)Math.Clamp(f * 255, 0, 255);
 
     public void Dispose()
     {
